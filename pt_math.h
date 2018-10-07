76a08573814816c121c373b3eb66e3af0fb1a14b
pmttavara <34119228+pmttavara@users.noreply.github.com>
2018-10-07 06:27:28 -0400

    Added optional range checking & precise `pow()`
    - Implemented range checking enabled by `#define PT_RANGE_CHECKS`, *greatly* easing use of `exp`/`log`/`pow`, `fmod`, `sinh`/`asinh`, etc. This involves branching, albeit small early-outs, so it's disabled by default.
    - For negative bases and/or exponents, and bases near 1 with huge exponents, `pow` can give very imprecise results. The library now has an implementation using exponentiation-by-squaring available via `#define PT_PRECISE_POW`. This library ostensibly offers simple approximations, but `pow` can be essential *while* tricky to get right, so this better slower alternative is offered.

--- pt_math.h
+++ pt_math.h
@@ -1,41 +1,44 @@
-/* pt_math.h - public domain branchless scalar math routines */
+/* pt_math.h - branchless scalar math routines */
+/* No attribution necessary; hyper-permissive license at bottom of file. */
 #ifndef PT_MATH_H
 #define PT_MATH_H
+/*
+#define PT_RANGE_CHECKS
+#define PT_PRECISE_POW
+*/
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef unsigned long long PT__ull;
-typedef long long PT__ll;
-#define PT__FPCOPY64(dst, src)                                                 \
+#define PT_FPCOPY64(dst, src)                                                  \
     do {                                                                       \
-        int PT__i;                                                             \
-        for (PT__i = 0; PT__i < 8; PT__i++) {                                  \
-            ((char *)&(dst))[PT__i] = ((char *)&(src))[PT__i];                 \
+        int PT_i;                                                              \
+        for (PT_i = 0; PT_i < 8; PT_i++) {                                     \
+            ((char *)&(dst))[PT_i] = ((char *)&(src))[PT_i];                   \
         }                                                                      \
     } while (0)
-#define PT__FPCOPY32(dst, src)                                                 \
+#define PT_FPCOPY32(dst, src)                                                  \
     do {                                                                       \
-        int PT__i;                                                             \
-        for (PT__i = 0; PT__i < 4; PT__i++) {                                  \
-            ((char *)&(dst))[PT__i] = ((char *)&(src))[PT__i];                 \
+        int PT_i;                                                              \
+        for (PT_i = 0; PT_i < 4; PT_i++) {                                     \
+            ((char *)&(dst))[PT_i] = ((char *)&(src))[PT_i];                   \
         }                                                                      \
     } while (0)
 
 static int PT_abs(int x) {
-    return (x < 0 ? -x : x);
+    return x < 0 ? -x : x;
 }
 static long PT_labs(long x) {
-    return (x < 0 ? -x : x);
+    return x < 0 ? -x : x;
 }
 static long long PT_llabs(long long x) {
-    return (x < 0 ? -x : x);
+    return x < 0 ? -x : x;
 }
 static double PT_fabs(double x) {
-    return (x < 0 ? -x : x);
+    return x < 0 ? -x : x;
 }
 static float PT_fabsf(float x) {
-    return (x < 0 ? -x : x);
+    return x < 0 ? -x : x;
 }
 #if defined(_MSC_VER) || defined(__GNUC__)
 #include <emmintrin.h>
@@ -43,73 +46,96 @@ static double PT_sqrt(double x) {
     __m128d m;
     m = _mm_set_sd(x);
     m = _mm_sqrt_pd(m);
-    x = _mm_cvtsd_f64(m);
-    return x;
+    return _mm_cvtsd_f64(m);
 }
 static float PT_sqrtf(float x) {
     __m128 m;
     m = _mm_set_ss(x);
     m = _mm_sqrt_ps(m);
-    x = _mm_cvtss_f32(m);
-    return x;
+    return _mm_cvtss_f32(m);
 }
 static double PT_rsqrt(double x) {
-    return 1 / PT_sqrt(x);
+    __m128d m;
+    m = _mm_set_sd(1 / x);
+    m = _mm_sqrt_pd(m);
+    return _mm_cvtsd_f64(m);
 }
 static float PT_rsqrtf(float x) {
     __m128 m;
     m = _mm_set_ss(x);
     m = _mm_rsqrt_ps(m);
-    x = _mm_cvtss_f32(m);
-    return x;
+    return _mm_cvtss_f32(m);
 }
 #else
 static double PT_sqrt(double x) {
-    PT__ll y;
+    long long y;
     double z;
-    PT__FPCOPY64(y, x);
+#ifdef PT_RANGE_CHECKS
+    if (x < 0)
+        return 1e300 * 1e300 * 0;
+#endif
+    PT_FPCOPY64(y, x);
     y = ((y - 0x0010000000000000ll) >> 1) + 0x2000000000000000ll;
-    PT__FPCOPY64(z, y);
-    z = (x / z + z) * 0.5;
+    PT_FPCOPY64(z, y);
     return (x / z + z) * 0.5;
 }
 static float PT_sqrtf(float x) {
     int y;
     float z;
-    PT__FPCOPY32(y, x);
+#ifdef PT_RANGE_CHECKS
+    if (x < 0)
+        return (float)(1e300 * 1e300 * 0);
+#endif
+    PT_FPCOPY32(y, x);
     y = ((y - 0x00800000) >> 1) + 0x20000000;
-    PT__FPCOPY32(z, y);
-    z = (x / z + z) * 0.5f;
+    PT_FPCOPY32(z, y);
     return (x / z + z) * 0.5f;
 }
 static double PT_rsqrt(double x) {
-    PT__ll y;
+    long long y;
     double z;
+#ifdef PT_RANGE_CHECKS
+    if (x == 0)
+        return 1e300 * 1e300;
+    if (x < 0)
+        return 1e300 * 1e300 * 0;
+#endif
     z = x * 0.5;
-    PT__FPCOPY64(y, x);
+    PT_FPCOPY64(y, x);
     y = 0x5fe6eb50c7b537a9 - (y >> 1);
-    PT__FPCOPY64(x, y);
-    x *= 1.5 - z * x * x;
+    PT_FPCOPY64(x, y);
     return x * (1.5 - z * x * x);
 }
 static float PT_rsqrtf(float x) {
     int y;
     double z;
+#ifdef PT_RANGE_CHECKS
+    if (x == 0)
+        return 1e300 * 1e300;
+    if (x < 0)
+        return (float)(1e300 * 1e300 * 0);
+#endif
     z = x * 0.5f;
-    PT__FPCOPY32(y, x);
+    PT_FPCOPY32(y, x);
     y = 0x5f375a86 - (y >> 1);
-    PT__FPCOPY32(x, y);
-    x *= 1.5f - z * x * x;
+    PT_FPCOPY32(x, y);
     return x * (1.5f - z * x * x);
 }
 #endif
-
 static double PT_round(double x) {
+#ifdef PT_RANGE_CHECKS
+    if ((x < 0 ? -x : x) > 4503599627370495.5)
+        return x;
+#endif
     x += 6755399441055744.0;
     x -= 6755399441055744.0;
     return x;
 }
 static float PT_roundf(float x) {
+#ifdef PT_RANGE_CHECKS
+    if ((x < 0 ? -x : x) > 8388607.5f)
+        return x;
+#endif
     x += 12582912.0f;
     x -= 12582912.0f;
     return x;
@@ -126,6 +152,26 @@ static double PT_ceil(double x) {
 static float PT_ceilf(float x) {
     return PT_roundf(x + 0.5f);
 }
+static double PT_trunc(double x) {
+#ifdef PT_RANGE_CHECKS
+    if (!((x < 0 ? -x : x) <= 4503599627370495.5))
+        return x;
+#endif
+    return (double)(long long)x;
+}
+static float PT_truncf(float x) {
+#ifdef PT_RANGE_CHECKS
+    if (!((x < 0 ? -x : x) <= 8388607.5f))
+        return x;
+#endif
+    return (float)(int)x;
+}
+static double PT_frac(double x) {
+    return x - PT_trunc(x);
+}
+static float PT_fracf(float x) {
+    return x - PT_truncf(x);
+}
 static double PT_remainder(double x, double y) {
     return x - PT_floor(x / y) * y;
 }
@@ -133,16 +179,18 @@ static float PT_remainderf(float x, float y) {
     return x - PT_floorf(x / y) * y;
 }
 static double PT_fmod(double x, double y) {
-    return x - (PT__ll)(x / y) * y;
+#ifdef PT_RANGE_CHECKS
+    if (y == 0)
+        return 1e300 * 1e300 * 0;
+#endif
+    return x - PT_trunc(x / y) * y;
 }
 static float PT_fmodf(float x, float y) {
-    return x - (PT__ll)(x / y) * y;
-}
-static double PT_trunc(double x) {
-    return (double)(PT__ll)x;
-}
-static float PT_truncf(float x) {
-    return (float)(PT__ll)x;
+#ifdef PT_RANGE_CHECKS
+    if (y == 0)
+        return (float)(1e300 * 1e300 * 0);
+#endif
+    return x - PT_truncf(x / y) * y;
 }
 static double PT_sin(double x) {
     double y;
@@ -212,47 +260,70 @@ static float PT_atan2f(float y, float x) {
 }
 static double PT_exp2(double x) {
     double y;
-    PT__ll exponent;
-    exponent = (PT__ll)(x + 1023);
+    long long exponent;
+#ifdef PT_RANGE_CHECKS
+    if (x <= -1022)
+        return 0;
+    if (x > 1024)
+        return 1e300 * 1e300;
+#endif
+    exponent = (long long)(x + 1023);
     x += 1023 - exponent;
     exponent <<= 52;
-    PT__FPCOPY64(y, exponent);
+    PT_FPCOPY64(y, exponent);
     x *= x * 0.339766027260413688582 + 0.660233972739586311418;
     return (x + 1) * y;
 }
 static float PT_exp2f(float x) {
     float y;
     int exponent;
+#ifdef PT_RANGE_CHECKS
+    if (x <= -126)
+        return 0;
+    if (x > 128)
+        return 1e300 * 1e300;
+#endif
     exponent = (int)(x + 127);
     x += 127 - exponent;
     exponent <<= 23;
-    PT__FPCOPY32(y, exponent);
+    PT_FPCOPY32(y, exponent);
     x *= x * 0.339766027f + 0.660233972f;
     return (x + 1) * y;
 }
 static double PT_log2(double x) {
-    PT__ll y;
+    long long y;
     double result;
-    PT__FPCOPY64(y, x);
+#ifdef PT_RANGE_CHECKS
+    if (x == 0)
+        return -1e300 * 1e300;
+    if (x < 0)
+        return 1e300 * 1e300 * 0;
+#endif
+    PT_FPCOPY64(y, x);
     y >>= 52;
     result = (double)y;
-    PT__FPCOPY64(y, x);
+    PT_FPCOPY64(y, x);
     y = y & 0x000fffffffffffff | 0x3ff0000000000000;
-    PT__FPCOPY64(x, y);
-    result = result - 1024 + x * (x * -0.33333333333333333 + 2) -
-             0.66666666666666666;
+    PT_FPCOPY64(x, y);
+    result += -1024 + x * (x * -0.33333333333333333 + 2) - 0.66666666666666666;
     return result;
 }
 static float PT_log2f(float x) {
     int y;
     float result;
-    PT__FPCOPY32(y, x);
+#ifdef PT_RANGE_CHECKS
+    if (x == 0)
+        return -1e300 * 1e300;
+    if (x < 0)
+        return (float)(1e300 * 1e300 * 0);
+#endif
+    PT_FPCOPY32(y, x);
     y >>= 23;
     result = (float)y;
-    PT__FPCOPY32(y, x);
+    PT_FPCOPY32(y, x);
     y = y & 0x007fffff | 0x3f800000;
-    PT__FPCOPY32(x, y);
-    result = result - 128 + x * (x * -0.333333333f + 2) - 0.666666666f;
+    PT_FPCOPY32(x, y);
+    result += -128 + x * (x * -0.333333333f + 2) - 0.666666666f;
     return result;
 }
 static double PT_exp(double x) {
@@ -280,10 +351,56 @@ static float PT_log10f(float x) {
     return PT_log2f(x) * 0.301029996f;
 }
 static double PT_pow(double a, double b) {
+#ifdef PT_PRECISE_POW
+    unsigned long long i;
+    if (b < 0)
+        return 1 / PT_pow(a, -b);
+    i = (unsigned long long)b;
+    b -= i;
+#ifdef PT_RANGE_CHECKS
+    if (a < 0)
+        if (b)
+            return 1e300 * 1e300 * 0;
+        else {
+            a = -a;
+            if (i & 1)
+                return -PT_pow(a, i);
+        }
+#endif
+    b = PT_exp2(b * PT_log2(a));
+    for (; i; a *= a, i >>= 1)
+        if (i & 1)
+            b *= a;
+    return b;
+#else
     return PT_exp2(b * PT_log2(a));
+#endif
 }
 static float PT_powf(float a, float b) {
+#ifdef PT_PRECISE_POW
+    unsigned int i;
+    if (b < 0)
+        return 1 / PT_powf(a, -b);
+    i = (unsigned int)b;
+    b -= i;
+#ifdef PT_RANGE_CHECKS
+    if (a < 0)
+        if (b)
+            return (float)(1e300 * 1e300 * 0);
+        else {
+            a = -a;
+            if (i & 1)
+                return -PT_powf(a, i);
+        }
+#endif
+    b = PT_exp2f(b * PT_log2f(a));
+    for (; i; a *= a, i >>= 1)
+        if (i & 1)
+            b *= a;
+    return b;
+#else
     return PT_exp2f(b * PT_log2f(a));
+#endif
 }
 static double PT_sinh(double x) {
     x = PT_exp(x);
@@ -316,9 +433,17 @@ static float PT_asinhf(float x) {
     return PT_logf(x + PT_sqrtf(x * x + 1));
 }
 static double PT_acosh(double x) {
+#ifdef PT_RANGE_CHECKS
+    if (x < 1)
+        return 1e300 * 1e300 * 0;
+#endif
     return PT_log(x + PT_sqrt(x * x - 1));
 }
 static float PT_acoshf(float x) {
+#ifdef PT_RANGE_CHECKS
+    if (x < 1)
+        return (float)(1e300 * 1e300 * 0);
+#endif
     return PT_logf(x + PT_sqrtf(x * x - 1));
 }
 static double PT_atanh(double x) {
@@ -328,10 +453,18 @@ static float PT_atanhf(float x) {
     return PT_logf((1 + x) / (1 - x)) * 0.5f;
 }
 static double PT_erf(double x) {
+#ifdef PT_RANGE_CHECKS
+    if (x >= 15.26)
+        return 1;
+#endif
     x = PT_exp2(x * 3.47203417614113462733);
     return x / ((x < 0 ? -x : x) + 1) * 2 - 1;
 }
 static float PT_erff(float x) {
+#ifdef PT_RANGE_CHECKS
+    if (x >= 6.912f)
+        return 1;
+#endif
     x = PT_exp2f(x * 3.472034176f);
     return x / ((x < 0 ? -x : x) + 1) * 2 - 1;
 }
@@ -344,26 +477,27 @@ static float PT_erfcf(float x) {
 #define PT_TAU 6.28318530717958648
 #define PT_E 2.71828182845904524
 #define PT_INFINITY ((float)(1e300 * 1e300))
-#define PT_NAN ((float)(PT_INFINITY * 0))
-#define PT_TAUf ((float)PT_TAU)
-#define PT_Ef ((float)PT_E)
+#define PT_NAN (PT_INFINITY * 0)
+
 #define PT_ABS(x) ((x) < 0 ? -(x) : (x))
+
+#define PT_isfinite(f) (PT_fpclassify(f) < PT_FP_INFINITE)
+#define PT_isinf(f) (PT_fpclassify(f) == PT_FP_INFINITE)
+#define PT_isnan(f) (PT_fpclassify(f) == PT_FP_NAN)
+#define PT_isnormal(f) (PT_fpclassify(f) == PT_FP_NORMAL)
+#define PT_signbit(f) (!!(PT_CLASS(f) & 8))
+
+#define PT_fpclassify(f) (PT_CLASS(f) & 7)
+
 #define PT_FP_ZERO 0
 #define PT_FP_SUBNORMAL 1
 #define PT_FP_NORMAL 2
 #define PT_FP_INFINITE 3
 #define PT_FP_NAN 4
-#define PT__CLASS(f) (sizeof(f) == 8 ? PT__dclass(f) : PT__fclass(f))
-#define PT_fpclassify(f) ((int)(PT__CLASS(f) & 7))
-#define PT_signbit(f) (!!(PT__CLASS(f) & 0x80000000u))
-#define PT_isfinite(f) (PT_fpclassify(f) < PT_FP_INFINITE)
-#define PT_isinf(f) (PT_fpclassify(f) == PT_FP_INFINITE)
-#define PT_isnan(f) (PT_fpclassify(f) == PT_FP_NAN)
-#define PT_isnormal(f) (PT_fpclassify(f) == PT_FP_NORMAL)
-static unsigned int PT__dclass(double x) {
-    PT__ull u;
-    PT__FPCOPY64(u, x);
-    unsigned int signbit = (u >> 32) & 0x80000000;
+static int PT_dclass(double x) {
+    long long u;
+    PT_FPCOPY64(u, x);
+    int signbit = (u >> 60) & 8;
     u &= 0x7fffffffffffffff;
     if (u == 0) {
         return signbit | PT_FP_ZERO;
@@ -379,10 +513,10 @@ static unsigned int PT__dclass(double x) {
     }
     return signbit | PT_FP_NAN;
 }
-static unsigned int PT__fclass(float x) {
-    unsigned int u;
-    PT__FPCOPY32(u, x);
-    unsigned int signbit = u & 0x80000000;
+static int PT_fclass(float x) {
+    int u;
+    PT_FPCOPY32(u, x);
+    int signbit = (u >> 28) & 8;
     u &= 0x7fffffff;
     if (u == 0) {
         return signbit | PT_FP_ZERO;
@@ -398,6 +532,7 @@ static unsigned int PT__fclass(float x) {
     }
     return signbit | PT_FP_NAN;
 }
+#define PT_CLASS(f) (sizeof(f) == 8 ? PT_dclass(f) : PT_fclass(f))
 #define PT_FNS(S, D, N)                                                        \
     S(abs, int)                                                                \
     S(labs, long)                                                              \
@@ -414,12 +549,14 @@ static unsigned int PT__fclass(float x) {
     S(floorf, float)                                                           \
     S(ceil, double)                                                            \
     S(ceilf, float)                                                            \
+    S(trunc, double)                                                           \
+    S(truncf, float)                                                           \
+    S(frac, double)                                                            \
+    S(fracf, float)                                                            \
     D(remainder, double)                                                       \
     D(remainderf, float)                                                       \
     D(fmod, double)                                                            \
     D(fmodf, float)                                                            \
-    S(trunc, double)                                                           \
-    S(truncf, float)                                                           \
     S(sin, double)                                                             \
     S(sinf, float)                                                             \
     S(cos, double)                                                             \
@@ -469,3 +606,23 @@ static unsigned int PT__fclass(float x) {
 }
 #endif
 #endif /* PT_MATH_H */
+
+/*
+Copyright (c) Phillip Trudeau-Tavara, 2017-2018
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
